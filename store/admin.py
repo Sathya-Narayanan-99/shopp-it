ee144a397922c46bfdb267f97d63ddbb5210f379
Sathya <nanasathya@gmail.com>
2021-04-24 11:15:22 +0530

    created superuser, added test products via admin site

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+#sathya
+#TestPass123
+
 # Register your models here.
-admin.site.admin.site.register(Customer)
-admin.site.admin.site.register(Product)
-admin.site.admin.site.register(Cart)
-admin.site.admin.site.register(CartItems)
-admin.site.admin.site.register(ShippingAddress)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItems)
+admin.site.register(ShippingAddress)
