1702f5e51bafd8803e7c5615017c80f7254b1a89
Sathya <nanasathya@gmail.com>
2021-04-24 09:50:57 +0530

    added CartItems model

--- store/models.py
+++ store/models.py
@@ -30,4 +30,11 @@ class Cart(models.Model):
     def __str__(self):
         return str(self.id)
 
+class CartItems(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.product.name
