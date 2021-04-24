1ae14c5c772a4722319007ffe02a7d6206d2587e
Sathya <nanasathya@gmail.com>
2021-04-24 08:44:03 +0530

    added Product  model

--- store/models.py
+++ store/models.py
@@ -10,3 +10,13 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
