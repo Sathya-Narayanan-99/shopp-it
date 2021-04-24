3533f680acec30e53a126159d9ca33dd6e8678e6
Sathya <nanasathya@gmail.com>
2021-04-24 08:48:28 +0530

    added Cart  model

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=200, null=True)
 
@@ -16,7 +16,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    #image
 
     def __str__(self):
         return self.name
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
 
